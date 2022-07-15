554abcadfc7b138af6161a438da864e896f0a9c7
Ryan Arya Pramudya <93182970+ryanaryap@users.noreply.github.com>
2022-07-15 21:37:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -599,4 +599,4 @@ def index():
 def error(e):
 	return render_template('error.html'), 404
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=int(os.environ.get('PORT','5000')),debug=False)
+	app.run(host='0.0.0.0', '127.0.0.7', 'asr-restapi.herokuapp.com', port=int(os.environ.get('PORT','5000')),debug=False)
